3a84915e6705eb934f4314e490c6e178c32ed45d
tivaty <tvtinh.documents@gmail.com>
2025-04-07 10:08:36 +0700

    adjust logic from A0 to D0

--- auto_fire_killer/auto_fire_killer.ino
+++ auto_fire_killer/auto_fire_killer.ino
@@ -1,14 +1,16 @@
 #include <Servo.h>
 
-#define FIRE_SENSOR_APIN A7  // Cảm biến lửa (Analog)
-#define FIRE_SENSOR_DPIN 4   // Cảm biến lửa (Digital)
+#define FIRE_SENSOR_APIN A6  // Cảm biến lửa (Analog)
+#define FIRE_SENSOR_DPIN A7   // Cảm biến lửa (Digital)
 #define RELAY_PIN 2          // Rơ-le báo động
 #define SERVO_PIN 3          // Servo
+#define START_ANGLE 90
 
 Servo myServo;
-int nguong = 600;  // Ngưỡng phát hiện lửa
+// int nguong = 600;  // Ngưỡng phát hiện lửa
+int nguong = 1;
 bool fireDetected = false; // Trạng thái có lửa hay không
-int angle = 0;      // Góc servo
+int angle = START_ANGLE;      // Góc servo
 bool increasing = true;  // Biến kiểm soát hướng quay
 
 void setup() {
@@ -18,16 +20,24 @@ void setup() {
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, LOW);
   myServo.attach(SERVO_PIN);
-  myServo.write(0);  // Servo bắt đầu từ vị trí 0 độ
+  myServo.write(angle);  // Servo bắt đầu từ vị trí start độ
+  digitalWrite(RELAY_PIN, HIGH);  // Bật rơ-le báo động
+  delay(1000);
+  digitalWrite(RELAY_PIN, LOW);  // Bật rơ-le báo động
+
 }
 
 void loop() {
   int fire_a = analogRead(FIRE_SENSOR_APIN);  // Đọc giá trị cảm biến lửa
-  Serial.print("fire_a = ");
+  int fire_d = digitalRead(FIRE_SENSOR_DPIN);
+  Serial.print("fire_d = ");
+  Serial.print(fire_d);
+  Serial.print(" | fire_a = ");
   Serial.println(fire_a);
 
   // Kiểm tra có lửa không
-  if (fire_a < nguong) {  
+  if (fire_d < nguong) {  
+  // if(fire_d == 1){
     Serial.println("🔥 Cảnh báo: Phát hiện lửa!");
     Serial.println("Máy bơm bật");
     digitalWrite(RELAY_PIN, HIGH);  // Bật rơ-le báo động
